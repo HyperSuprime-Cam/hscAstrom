4ed60ef1c097312b43174dd7b27a7e07e713d31d
Russell Owen <rowen@uw.edu>
2015-02-20 14:00:21 -0800

    Add unit test for fitTAN and improve unit test for fitTANSIP
    The unit test for fitTANSIP was expecting the SIP terms to take out
    far too much offset and scale change. Now the test only fails
    for a quadratic distortion. I'm still puzzled by that failure.
    
    The new test of fitTAN fails even when no distortion is added.
    I'm not sure if the problem is in the test or the code being tested.
    (And yes, there is far too much duplication between these tests).

--- tests/testFitTAN.py
+++ tests/testFitTAN.py
@@ -21,10 +21,10 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
  
 """
-Tests for fitTANSIP
+Tests for fitTAN
 
 Run with:
-   testFitTANSIP.py
+   testFitTAN.py
 """
 import unittest
 
@@ -41,15 +41,15 @@ import lsst.afw.coord as afwCoord
 import lsst.afw.geom as afwGeom
 import lsst.afw.image as afwImage
 import lsst.afw.table as afwTable
+from lsst.meas.base import SingleFrameMeasurementTask
 import hsc.meas.astrom as hscAstrom
 
 from lsst.pex.logging import Log
 log = Log.getDefaultLog()
 #log.setThreshold(Log.DEBUG)
 
-
-class FitTANSIPTestCase(unittest.TestCase):
-    """A test case for fitTANSIP
+class FitTanTestCase(unittest.TestCase):
+    """A test case for fitTanWcs
     """
     def setUp(self):
         self.crCoord = afwCoord.IcrsCoord(afwGeom.PointD(44., 45.))
@@ -66,7 +66,8 @@ class FitTANSIPTestCase(unittest.TestCase):
         S = 3000
         N = 4
 
-        refTable = afwTable.SimpleTable.make(afwTable.SimpleTable.makeMinimalSchema())
+        posRefSchema = afwTable.SimpleTable.makeMinimalSchema()
+        posRefTable = afwTable.SimpleTable.make(posRefSchema)
         srcSchema = afwTable.SourceTable.makeMinimalSchema()
         self.srcCentroidKey = srcSchema.addField("centroid", type="PointD")
         srcSchema.addField("centroid.flags", type="Flag")
@@ -78,14 +79,14 @@ class FitTANSIPTestCase(unittest.TestCase):
         for i in numpy.linspace(0., S, N):
             for j in numpy.linspace(0., S, N):
                 src = srcTable.makeRecord()
-                refObj = refTable.makeRecord()
+                refObj = posRefTable.makeRecord()
 
                 src.set(self.srcCentroidKey.getX(), i)
                 src.set(self.srcCentroidKey.getY(), j)
                 src.set(self.srcCentroidErrKey[0, 0], 0.1)
                 src.set(self.srcCentroidErrKey[1, 1], 0.1)
 
-                c = self.tanWcs.pixelToSky(afwGeom.Point2D(i, j))
+                c = self.tanWcs.pixelToSky(i, j)
                 refObj.setCoord(c)
                 
                 if False:
@@ -102,13 +103,21 @@ class FitTANSIPTestCase(unittest.TestCase):
         """Add no distortion"""
         self.doTest("testTrivial", lambda x, y: (x, y))
 
-    def testScaleXY(self):
-        """Scale x and y slightly"""
-        self.doTest("testScaleXY", lambda x, y: (1.002*x, 0.998 * y))
+    def testOffset(self):
+        """Add an offset"""
+        self.doTest("testOffset", lambda x, y: (x + 5, y + 7))
 
-    def testQuadraticX(self):
-        """Add quadratic distortion in x"""
-        self.doTest("testQuadraticX", lambda x, y: (x + 1e-5*x**2, y), order=3)
+    def testLinearX(self):
+        """Scale x, offset y"""
+        self.doTest("testLinearX", lambda x, y: (2*x, y + 7))
+
+    def testLinearXY(self):
+        """Scale x and y"""
+        self.doTest("testLinearXY", lambda x, y: (2*x, 3*y))
+
+    def testLinearYX(self):
+        """Add an offset to each point; scale in y and x"""
+        self.doTest("testLinearYX", lambda x, y: (x + 0.2*y, y + 0.3*x))
 
     def checkResults(self, tanSipWcs):
         for refObj, src, d in self.matchList:
@@ -132,7 +141,7 @@ class FitTANSIPTestCase(unittest.TestCase):
             self.assertAlmostEqual(srcPixPos[0], refPixPos[0], 3) # within a milli-pixel
             self.assertAlmostEqual(srcPixPos[1], refPixPos[1], 3)
 
-    def doTest(self, name, func, order=2):
+    def doTest(self, name, func):
         """Apply func(x, y) to each source in self.srcCat, then fit and check the resulting WCS
         """
         for refObj, src, d in self.matchList:
@@ -142,7 +151,7 @@ class FitTANSIPTestCase(unittest.TestCase):
             src.set(self.srcCentroidKey.getX(), x)
             src.set(self.srcCentroidKey.getY(), y)
 
-        tanSipWcs = hscAstrom.fitTANSIP(order, self.matchList, self.crCoord, self.crPix)
+        tanSipWcs = hscAstrom.fitTAN(self.matchList)
 
         if False:
             self.plotWcs(tanSipWcs, name=name)
@@ -151,7 +160,7 @@ class FitTANSIPTestCase(unittest.TestCase):
 
     def plotWcs(self, tanSipWcs, name=""):
         pnum = 1
-        fileNamePrefix = "testFitTANSIP_%s" % (name,)
+        fileNamePrefix = "testFitTanWcs_%s" % (name,)
 
         xs,ys, xc,yc = [],[],[],[]
         rs,ds, rc,dc = [],[],[],[]
@@ -214,6 +223,7 @@ class FitTANSIPTestCase(unittest.TestCase):
         pylab.savefig(fileName)
         print "Wrote", fileName
         pnum += 1
+    
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -222,7 +232,7 @@ def suite():
     tests.init()
 
     suites = []
-    suites += unittest.makeSuite(FitTANSIPTestCase)
+    suites += unittest.makeSuite(FitTanTestCase)
     suites += unittest.makeSuite(tests.MemoryTestCase)
     return unittest.TestSuite(suites)
 
